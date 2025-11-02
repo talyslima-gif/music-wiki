b838a9cc3850aa33991b52792c2340e1a1029c88
Talys Lima <Talys.lima@aluno.ifsertao-pe.edu.br>
2025-11-02 13:15:23 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-let imgEl = document.querySelector("")
+document.body.style.opacity = 0;
+window.onload = () => {
+  document.body.style.transition = 'opacity 0.5s';
+  document.body.style.opacity = 1;
+};
